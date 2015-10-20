9992f651af80b03993f76b7501c0d21f89fbad17
saadzafar7223 <saad.zafar.7223@gmail.com>
2015-10-20 10:04:37 +0800

    Initial commit

---
+++ app/src/main/java/com/sgh/swinburne/heartplus/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.sgh.swinburne.heartplus.app;
+
+/**
+ * Created by Saad on 10/7/2015.
+ */
+public class AppConfig
+{
+    // Server user login url
+    public static String URL_LOGIN = "http://10.0.2.15:/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://10.0.2.15/android_login_api/register.php";
+}
