733d66698688a3a7c9f081831997d29ff17ae62a
fazy95 <faizanelahi95@gmail.com>
2016-04-26 01:05:56 +0800

    Pill reminder redesigned and re coded

--- app/src/main/java/com/sgh/swinburne/heartplus/pillreminder/PillListActivity.java
+++ app/src/main/java/com/sgh/swinburne/heartplus/pillreminder/PillListActivity.java
@@ -1,123 +1,200 @@
 package com.sgh.swinburne.heartplus.pillreminder;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.ContextMenu;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import com.sgh.swinburne.heartplus.MainActivity;
+import com.sgh.swinburne.heartplus.pillreminder.Alarm;
+import com.sgh.swinburne.heartplus.pillreminder.Pill;
+
+
+import com.sgh.swinburne.heartplus.pillreminder.adapter.ExpandableListAdapter;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
 
 import com.sgh.swinburne.heartplus.R;
 
+
 /**
- * Created by VivekShah.
+ * Created by Faizan.
  */
 
 /*Tap a class to edit it or long press to delete it*/
 
-public class PillListActivity extends ListActivity
-{
-    private static final int ACTIVITIES_CREATE=0;
-    private static final int ACTIVITIES_EDIT=1;
 
-    private PillDbAdapter DatabaseHelper;  //class level pillDbAdapter instance variable
+
+/**
+ * This activity handles the view and controller of the pillbox page, where
+ * the user can view alarms by pills and edit or delete an alarm
+ */
+
+
+public class PillListActivity extends Activity {
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+    // This data structure allows us to get the ids of the alarms we want to edit
+    // and store them in the tempId in the pill box model. The structure is similar
+    // to the struture of listDataChild.
+    List<List<List<Long>>> alarmIDData;
 
     @Override
-    // Save the values to the database
-    public void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pill_list);
-        DatabaseHelper = new PillDbAdapter(this); // PillDbAdapter in instantiated here
-        DatabaseHelper.open(); // open database
-        filltheData(); //loads data from db to listview
-        registerForContextMenu(getListView());
-    }
 
-    private void filltheData() {
-        Cursor pillCursor = DatabaseHelper.fetchAllPills();
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
 
-        startManagingCursor(pillCursor);
+        try {
+            prepareListData();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
 
-        // Create an array to specify the fields we want to display in the list (only TITLE)
-        String[] frm = new String[]{PillDbAdapter.KEY_NAME}; //selection criteria for the query i.e. return task title
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+        expListView.setAdapter(listAdapter);
 
-        // and an array of the fields we want to bind those fields to (in this case just text1)
-        int[] _to = new int[]{R.id.text1};
+        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        // Now create a simple cursor adapter and set it to display
-        SimpleCursorAdapter reminders =
-                new SimpleCursorAdapter(this, R.layout.pill_row, pillCursor, frm, _to); //map columns from cursor to textview like layout file
-        setListAdapter(reminders);//inform list of data
+        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expListView.setOnChildClickListener(new OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                PillBox pillBox = new PillBox();
+                pillBox.setTempIds(alarmIDData.get(groupPosition).get(childPosition));
+
+                Intent intent = new Intent(getApplicationContext(), PillEditActivity.class);
+                startActivity(intent);
+                finish();
+                return false;
+            }
+        });
     }
 
     @Override
+    /** Inflate the menu; this adds items to the action bar if it is present */
     public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        MenuInflater mi = getMenuInflater();
-        mi.inflate(R.menu.item_menu_list, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onMenuItemSelected(int feature_Id, MenuItem itm) {
-        switch(itm.getItemId()) {
-            case R.id.m_insert:
-                create_Reminder();
-                return true;
-            case R.id.m_settings:
-                Intent intnt = new Intent(this, TaskPref.class);
-                startActivity(intnt);
-                return true;
+    /**
+     * Handle action bar item clicks here. The action bar will
+     * automatically handle clicks on the Home/Up button, so long
+     * as you specify a parent activity in AndroidManifest.xml.
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_add) {
+            sendAdd();
+            return true;
         }
 
-        return super.onMenuItemSelected(feature_Id, itm);
+       /* if (id == R.id.action_History) {
+            sendHistory();
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onCreateContextMenu(ContextMenu _menu, View view, ContextMenu.ContextMenuInfo _menuInfo) {
-        super.onCreateContextMenu(_menu, view, _menuInfo);
-        MenuInflater minf = getMenuInflater();
-        minf.inflate(R.menu.item_logpressed, _menu);
-    }
 
-    @Override
-    public boolean onContextItemSelected(MenuItem _item) //defines method handling menu events such as longpress on list view
-    {
-        switch(_item.getItemId()) {
-            case R.id.menu_delete:
-                AdapterView.AdapterContextMenuInfo inform = (AdapterView.AdapterContextMenuInfo) _item.getMenuInfo(); //method getmenuinfo() obtain instance of adaptercontextmenuinfo on the clicked item.
-                DatabaseHelper.deletePill(inform.id);//delete task of id..which came from adaptercontextmenu
-                filltheData();//refresh list view
-                return true;
+    /** Preparing the list data */
+    private void prepareListData() throws URISyntaxException {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+        alarmIDData = new ArrayList<List<List<Long>>>();
+
+        PillBox pillbox = new PillBox();
+        List<Pill> pills = pillbox.getPills(this);
+        Collections.sort(pills, new PillComparator());
+
+        for (Pill pill: pills){
+            String name = pill.getPillName();
+            String dose = pill.getDose();
+            String instruction = pill.getInstruction();
+            listDataHeader.add(name);
+            List<String> times = new ArrayList<String>();
+            List<Alarm> alarms = pillbox.getAlarmByPill(this.getBaseContext(), name);
+            List<List<Long>> ids = new ArrayList<List<Long>>();
+
+            for (Alarm alarm :alarms){
+                String time = alarm.getStringTime() + daysList(alarm);
+                times.add(time);
+                ids.add(alarm.getIds());
+            }
+            alarmIDData.add(ids);
+            listDataChild.put(name, times);
         }
-        return super.onContextItemSelected(_item);
     }
 
-    private void create_Reminder()
-    {
-        Intent intent = new Intent(this, PillEditActivity.class);
-        startActivityForResult(intent, ACTIVITIES_CREATE);
+    /**
+     * Helper function to obtain a string of the days of the week
+     * that can be used as a boolean list
+     */
+    private String daysList(Alarm alarm){
+        String days = "#";
+        for(int i=0; i<7; i++){
+            if (alarm.getDayOfWeek()[i])
+                days += "1";
+            else
+                days += "0";
+        }
+        return days;
     }
+
+    public void sendAdd() {
+        Intent intent = new Intent(this, AddActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /*public void sendHistory() {
+        Intent intent = new Intent(this, HistoryFragment.class);
+        startActivity(intent);
+        finish();
+    }*/
 
     @Override
-    protected void onListItemClick(ListView lv, View view, int posi, long _id) {
-        super.onListItemClick(lv, view, posi, _id);
-        Intent intent = new Intent(this, PillEditActivity.class);
-        intent.putExtra(PillDbAdapter.KEY_RID, _id); //gives id of reminder being edited
-        startActivityForResult(intent, ACTIVITIES_EDIT);
-    }
-
-    @Override
-    protected void onActivityResult(int reqCode, int resCode, Intent _intent) //result_ok inspected here
-    {
-        super.onActivityResult(reqCode, resCode, _intent);
-        filltheData();//method called when returning from another activity in the case of update or edit in the reminder.
+    public void onBackPressed() {
+        Intent returnHome = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(returnHome);
+        finish();
     }
 }
